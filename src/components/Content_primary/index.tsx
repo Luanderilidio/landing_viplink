818aa2d9293877df178698194e8f20b29c359f0f
luanderilidio <luanderilidio174@gmail.com>
2023-04-28 18:35:08 -0400

    feat: implements layout responsive

--- src/components/Content_primary/index.tsx
+++ src/components/Content_primary/index.tsx
@@ -2,65 +2,75 @@ import "animate.css";
 
 export default function Content_Primary() {
   return (
-    <div className="grid grid-cols-12 gap-10 font-Inter mt-10 px-10">
-      <div className="col-span-5 flex flex-col gap-6">
-        <h1 className="text-8xl font-bold font-Inter	">
+    <div className=" grid grid-cols-12 sm:p-10 mt-2">
+      <div className="col-span-12 sm:col-span-6 flex flex-col gap-7">
+        <h1 className="text-5xl text-center sm:text-left sm:text-8xl font-bold font-Inter	">
           Brand
           <br />
           Growth for
           <br />
-          <span className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent font-Inter text-8xl font-black">
+          <span className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent font-Inter text-5xl sm:text-8xl font-black">
             Gen-Z
           </span>
         </h1>
-        <p>
-          <strong>Viplink.ai</strong> is a brand growth platform that uses
-          proprietary dynamic pricing and a broad network of professional
-          creators to help marketers and their agencies successfully drive brand
-          growth across social media platforms.
-        </p>
-        <div className="flex gap-5">
-          <button className="btn btn-active bg-violet-700 hover:bg-violet-600 active:bg-violet-700 shadow-lg shadow-violet-700/30 rounded-xl capitalize">
+        <div className="w-full flex items-center sm:justify-start justify-center">
+          <p className="w-4/5 sm:text-left text-center">
+            <strong>Viplink.ai</strong> is a brand growth platform that uses
+            proprietary dynamic pricing and a broad network of professional
+          </p>
+        </div>
+        <div className="hidden sm:flex items-center justify-start gap-5 ">
+          <button className="btn w-1/4 btn-active bg-violet-700 hover:bg-violet-600 active:bg-violet-700 shadow-lg shadow-violet-700/30 rounded-xl capitalize">
             Contat Us
           </button>
-          <button className="btn btn-outline border-2 border-white hover:border-white hover:bg-transparent rounded-xl text-white  capitalize">
+          <button className="btn w-1/4 btn-outline border-2 border-white hover:border-white hover:bg-transparent rounded-xl text-white  capitalize">
             Explore More
           </button>
         </div>
       </div>
-      <div className="col-span-7 flex w-full justify-center items-start  gap-5 rounded-xl ">
-        <div className="w-52 mt-32 animate__animated animate__fadeIn animate__slower">
-          <video
-            width="100%"
-            height="100%"
-            className="rounded-xl shadow-2xl shadow-black/70 transition-all ease-in-out cursor-pointer active:scale-95 hover:scale-105"
-            src="https://tamovip-itens.s3.sa-east-1.amazonaws.com/extras/challenges/99moto_1.mp4"
-            muted
-            loop
-            autoPlay
-          />
+      <div className="col-span-12 sm:col-span-6 gap-10 mt-10 sm:mt-0 flex flex-col justify-center ">
+        <div className="flex gap-5 justify-center">
+          <div className="w-24 sm:w-60 mt-5 sm:mt-20 animate__animated animate__fadeIn animate__slower">
+            <video
+              width="100%"
+              height="100%"
+              className="rounded-xl shadow-2xl shadow-black/70 transition-all ease-in-out cursor-pointer active:scale-95 hover:scale-105"
+              src="https://tamovip-itens.s3.sa-east-1.amazonaws.com/extras/challenges/99moto_1.mp4"
+              muted
+              loop
+              autoPlay
+            />
+          </div>
+          <div className="w-24 sm:w-60  animate__animated animate__fadeIn  animate__slower">
+            <video
+              width="100%"
+              height="100%"
+              className="rounded-xl shadow-2xl shadow-black/70 transition-all ease-in-out cursor-pointer active:scale-95 hover:scale-105"
+              src="https://tamovip-itens.s3.sa-east-1.amazonaws.com/extras/challenges/fusion_1.mp4"
+              muted
+              loop
+              autoPlay
+            />
+          </div>
+          <div className="w-24 sm:w-60 mt-5 sm:mt-20 animate__animated animate__fadeIn animate__slower">
+            <video
+              width="100%"
+              height="100%"
+              className="rounded-xl shadow-2xl shadow-black/70 transition-all ease-in-out cursor-pointer active:scale-95 hover:scale-105"
+              src="https://tamovip-itens.s3.sa-east-1.amazonaws.com/extras/challenges/wpt_1.mp4"
+              muted
+              loop
+              autoPlay
+            />
+          </div>
         </div>
-        <div className="w-60 animate__animated animate__fadeIn  animate__slower">
-          <video
-            width="100%"
-            height="100%"
-            className="rounded-xl shadow-2xl shadow-black/70 transition-all ease-in-out cursor-pointer active:scale-95 hover:scale-105"
-            src="https://tamovip-itens.s3.sa-east-1.amazonaws.com/extras/challenges/fusion_1.mp4"
-            muted
-            loop
-            autoPlay
-          />
-        </div>
-        <div className="w-52 mt-32 animate__animated animate__fadeIn animate__slower">
-          <video
-            width="100%"
-            height="100%"
-            className="rounded-xl shadow-2xl shadow-black/70 transition-all ease-in-out cursor-pointer active:scale-95 hover:scale-105"
-            src="https://tamovip-itens.s3.sa-east-1.amazonaws.com/extras/challenges/wpt_1.mp4"
-            muted
-            loop
-            autoPlay
-          />
+        <div className="flex sm:hidden items-center justify-evenly gap-5 ">
+          <button className="btn w-1/3 btn-active bg-violet-700 hover:bg-violet-600 active:bg-violet-700 shadow-lg shadow-violet-700/30 rounded-xl capitalize">
+            Contat Us
+          </button>
+          <button className="btn w-1/3 btn-outline border-2 border-white hover:border-white hover:bg-transparent rounded-xl text-white  capitalize">
+            Explore More
+          </button>
         </div>
       </div>
     </div>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,10 @@
+import Drawer from "react-modern-drawer";
+import "react-modern-drawer/dist/index.css";
 import LogoViplink from "../../assets/logo_viplink.png";
 import FlagBR from "../../assets/flags/BR.png";
 import FlagUS from "../../assets/flags/US.png";
 
-import { ArrowRight, CaretDown, Translate } from "phosphor-react";
+import { ArrowRight, CaretDown, List, Translate, X } from "phosphor-react";
 import { useState } from "react";
 
 export default function Header() {
@@ -15,50 +17,103 @@ export default function Header() {
   const handleMenu = () => setShowMenu(!showMenu);
 
   return (
-    <div className="w-full flex items-center justify-between font-Inter p-6">
-      <img alt="logo_viplink" className="w-28" src={LogoViplink} />
-      <div className="flex items-center justify-center gap-10 ">
-        <div className="flex flex-col group hover:text-[#4F46E5] transition-all ease-in-out  active:scale-95">
-          <button className=" py-1 text-sm font-Inter font-bold">
-            For influencers
-          </button>
-          <div className="transition-all ease-in-out w-0 group-hover:w-full h-1 bg-[#4F46E5]" />
-        </div>
-        <div className="flex flex-col group hover:text-[#4F46E5] transition-all ease-in  active:scale-95">
-          <button className="py-1 text-sm font-Inter font-bold">
-            About us
-          </button>
-          <div className="transition-all ease-in-out w-0 group-hover:w-full h-1 bg-[#4F46E5]" />
-        </div>
-
-        <div className="dropdown dropdown-hover dropdown-bottom dropdown-end">
-          <div className="transition ease-in-out flex items-center text-white hover:text-[#4F46E5]">
-            <Translate size={22} weight="bold" tabIndex={0} />
-            <CaretDown size={12} weight="bold" />
+    <>
+      <div className="w-full flex items-center justify-between font-Inter p-6">
+        <img alt="logo_viplink" className="w-28" src={LogoViplink} />
+        <div className="flex items-center justify-center gap-10 ">
+          <div className="hidden sm:block flex-col group hover:text-[#4F46E5] transition-all ease-in-out  active:scale-95">
+            <button className=" py-1 text-sm font-Inter font-bold">
+              For influencers
+            </button>
+            <div className="transition-all ease-in-out w-0 group-hover:w-full h-1 bg-[#4F46E5]" />
           </div>
-          <div
-            className=" text-black  p-2 shadow bg-base-100 rounded-lg  flex flex-col  dropdown-content menu w-32"
-            tabIndex={0}
+          <div className=" hidden sm:block flex-col group hover:text-[#4F46E5] transition-all ease-in  active:scale-95">
+            <button className="py-1 text-sm font-Inter font-bold">
+              About us
+            </button>
+            <div className="transition-all ease-in-out w-0 group-hover:w-full h-1 bg-[#4F46E5]" />
+          </div>
+          <div className="dropdown dropdown-hover dropdown-bottom dropdown-end">
+            <div className="transition ease-in-out flex items-center text-white hover:text-[#4F46E5]">
+              <Translate size={22} weight="bold" tabIndex={0} />
+              <CaretDown size={12} weight="bold" />
+            </div>
+            <div
+              className=" text-black  p-2 shadow bg-base-100 rounded-lg  flex flex-col  dropdown-content menu w-32"
+              tabIndex={0}
+            >
+              <button className=" transition-colors flex items-center  gap-2 rounded-lg   hover:bg-slate-100 p-3 active:bg-slate-200">
+                <img src={FlagUS} />
+                <p className="w-full font-Inter text-left font-medium text-xs">
+                  English
+                </p>
+              </button>
+              <button className=" transition-colors flex items-center gap-2 rounded-lg  hover:bg-slate-100 p-3 active:bg-slate-200">
+                <img src={FlagBR} />
+                <p className="w-full font-Inter text-left font-medium text-xs">
+                  Portuguese
+                </p>
+              </button>
+            </div>
+          </div>
+          <button className=" hidden btn bg-violet-700 hover:bg-violet-600 active:bg-violet-700 shadow-lg shadow-violet-700/30 text-white rounded-xl sm:flex items-center gap-2 text-sm capitalize">
+            Login
+            <ArrowRight weight="bold" size={18} />
+          </button>
+          <button
+            onClick={toggleDrawer}
+            className="flex sm:hidden transition ease-in-out active:scale-95"
           >
-            <button className=" transition-colors flex items-center  gap-2 rounded-lg   hover:bg-slate-100 p-3 active:bg-slate-200">
-              <img src={FlagUS} />
-              <p className="w-full font-Inter text-left font-medium text-xs">
-                English
-              </p>
-            </button>
-            <button className=" transition-colors flex items-center gap-2 rounded-lg  hover:bg-slate-100 p-3 active:bg-slate-200">
-              <img src={FlagBR} />
-              <p className="w-full font-Inter text-left font-medium text-xs">
-                Portuguese
-              </p>
-            </button>
+            <List size={27} weight="bold" />
+          </button>
+        </div>
+      </div>
+      <Drawer
+        open={isOpen}
+        onClose={toggleDrawer}
+        direction="right"
+        className="w-full bg-black"
+        size="100%"
+        style={{ background: "#141943" }}
+      >
+        <div className="h-screen w-screen p-6 font-Inter">
+          <div className="flex items-center justify-between">
+            <img alt="logo_viplink" className="w-28" src={LogoViplink} />
+            <X
+              onClick={toggleDrawer}
+              className="transition-colors ease-in-out hover:text-red-600"
+              size={32}
+              weight="bold"
+            />
+          </div>
+          <div className="w-full flex h-screen flex-col items-center justify-evenly gap-5">
+            <div />
+            <div className="flex flex-col gap-6 items-center justify-center">
+              <div className=" flex-col group hover:text-[#4F46E5] transition-all ease-in-out  active:scale-95">
+                <button className=" py-1 text-2xl font-Inter font-bold">
+                  For influencers
+                </button>
+                <div className="transition-all ease-in-out w-0 group-hover:w-full h-1 bg-[#4F46E5]" />
+              </div>
+              <div className="flex-col group hover:text-[#4F46E5] transition-all ease-in active:scale-95">
+                <button className="py-1 text-2xl font-Inter font-bold">
+                  About us
+                </button>
+                <div className="transition-all ease-in-out w-0 group-hover:w-full h-1 bg-[#4F46E5]" />
+              </div>
+            </div>
+            <div className="w-full gap-7 flex flex-col items-center justify-center">
+              <button className="btn btn-outline text-lg w-full border-violet-700 hover:border-violet-700 hover:bg-transparent active:bg-transparent shadow-lg hover:shadow-violet-700/40 shadow-violet-700/20 text-violet-700 rounded-xl sm:flex items-center gap-2 capitalize">
+                Sign Up
+              </button>
+              <button className="btn  text-lg w-full bg-violet-700 border-violet-700 hover:bg-violet-600 active:bg-violet-700 shadow-lg hover:shadow-violet-700/40 shadow-violet-700/20 text-white rounded-xl sm:flex items-center gap-2 capitalize">
+                Sign In
+              </button>
+            </div>
+            <div />
           </div>
         </div>
-        <button className="btn bg-violet-700 hover:bg-violet-600 active:bg-violet-700 shadow-lg shadow-violet-700/30 text-white  rounded-xl flex items-center gap-2 text-sm capitalize">
-          Login
-          <ArrowRight weight="bold" size={18} />
-        </button>
-      </div>
-    </div>
+      </Drawer>
+    </>
   );
 }
